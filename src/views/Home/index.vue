64bdfba57e20a91166c35b666beb9ee7f9627b81
xiaocheng <jincheng0609@gmail.com>
2023-05-18 16:32:30 +0800

    静态模板

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,6 +5,8 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
+
 </script>
 
 <template>
@@ -15,4 +17,13 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+
+  <!-- 非复杂的模板抽象成props，复杂的结构模板抽象为插槽 -->
+  <!-- 测试 -->
+  <HomePanel title="新鲜好恶物" subtitle="绝对新鲜">
+    <div>我是新鲜</div>
+  </HomePanel>
+  <HomePanel title="人气爆满" subtitle="非常昊">
+    <div>我是人气爆满</div>
+  </HomePanel>
 </template>
